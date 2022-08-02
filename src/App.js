2e3b4c7968953ef99ede193fda19d67567550972
arafatanjan <arafatanjan@gmail.com>
2022-08-02 14:28:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import initializeAuthentication from './Firebase/firebse.initialize';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+import { useState } from 'react';
+
+
+initializeAuthentication();
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+const auth = getAuth()
 
 function App() {
+  const [user, setUser] = useState({})
+  const handleGoogleSignIn = () => {
+
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user
+        const loginuser = {
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(loginuser);
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
+  }
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const user = result.user
+        console.log(user);
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}> Google Sign in</button>
+      <button onClick={handleGithubSignIn}> Github Sign in</button>
+      <br />
+      {user.email && <div>
+        <h2>{user.name}</h2>
+      </div>}
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB6QuYijIaMZG6iW-8KqlZDCz-OoLt_T_I",
+    authDomain: "doctors-site-f9de7.firebaseapp.com",
+    projectId: "doctors-site-f9de7",
+    storageBucket: "doctors-site-f9de7.appspot.com",
+    messagingSenderId: "28678455653",
+    appId: "1:28678455653:web:81619b51570ab3d54fe18c",
+    measurementId: "G-3RMD4NGCV0"
+};
+
+export default firebaseConfig;
