141d7c5cc44342152a4a16e198ddffead2b9eb23
arafatanjan <arafatanjan@gmail.com>
2022-08-08 23:35:43 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import About from './components/About/About';
-import Home from './components/Home/Home';
-import Notfound from './components/Notfound/Notfound';
-import Services from './components/Services/Services';
+import About from './Components/About/About';
+import Home from './Components/Home/Home';
+import Notfound from './Components/Notfound/Notfound';
+import Services from './Components/Services/Services';
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 import Warranty from './Components/Warranty/Warranty';
 import Newpatients from './Components/Newpatients/Newpatients';
 
