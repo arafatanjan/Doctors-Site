31968bbc8f0c91d8693bfb6bab9d2a22285feaa6
arafatanjan <arafatanjan@gmail.com>
2022-08-13 22:50:38 +0600

    first commit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,16 +6,16 @@ const Footer = () => {
         <div className="container text-center">
             <div className="row">
                 <div className="col">
-                    <h1>Frequently Asked Questions</h1>
-                    <h5>More questions? We are  ready to help. Give us a call at (303) 220-7662 or book your free consultation now.</h5>
+                    <h4>Frequently Asked Questions</h4>
+                    <h6>More questions? We are  ready to help. Give us a call at (303) 220-7662 or book your free consultation now.</h6>
                 </div>
                 <div className="col"><div className="accordion" id="accordionExample">
                     <div className="accordion-item">
-                        <h2 className="accordion-header" id="headingOne">
+                        <h6 className="accordion-header" id="headingOne">
                             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                 Accordion Item #1
                             </button>
-                        </h2>
+                        </h6>
                         <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                             <div className="accordion-body">
                                 <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
@@ -23,11 +23,11 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className="accordion-item">
-                        <h4 className="accordion-header" id="headingTwo">
+                        <h6 className="accordion-header" id="headingTwo">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                                 Accordion Item #2
                             </button>
-                        </h4>
+                        </h6>
                         <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                             <div className="accordion-body">
                                 <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
@@ -35,14 +35,15 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className="accordion-item">
-                        <h2 className="accordion-header" id="headingThree">
+                        <h6 className="accordion-header" id="headingThree">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                                 Accordion Item #3
                             </button>
-                        </h2>
+                        </h6>
                         <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                             <div className="accordion-body">
-                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <strong>This is the third item's accordion body.</strong> <p>
+                                    It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit </p>.
                             </div>
                         </div>
                     </div>
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from './service';
 
 const Services = () => {
-    return (
-        <div>
+    const [services, setServices] = useState([])
 
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, []);
+
+
+
+    return (
+        <div className='service-container'>
+
+            {
+                services.map(service => <Service></Service>)
+            }
         </div>
     );
 };
