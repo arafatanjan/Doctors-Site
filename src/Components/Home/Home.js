ab2fe1ea3f91c239959e157303b54a2083fb8375
arafatanjan <arafatanjan@gmail.com>
2022-08-18 20:40:03 +0600

    first commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
+import dental from '../../../src/pictures/village-dental-dentists.jpg';
+import './Home.css';
 
 const Home = () => {
     return (
-        <Container ><div className='fluid text-justify top-item p-2'>
-            <br />
-            <br />
-            <h1>Dentistry Done Right</h1>
-            <br />
+        <div className='home-container row' >
+            <div className='col'>
+                <br />
+                <br />
+                <h1>Dentistry Done Right</h1>
+                <br />
 
-            <h6>Adults and kids, we welcome patients of all ages! Our<br /> team is passionate about building lifetime relationships <br />through positive experiences, featuring:</h6>
-            <h6>Transparent Pricing</h6>
-            <h6>Unparalleled Warranty</h6>
-            <h6>FREE Whitening (for life!)</h6>
-            <br />
-            <br />
-            <h6>“Our word is our worth. We promise to do it right, timely, and for a fair price.”</h6>
-            <br />
-            <br />
+                <h6>Adults and kids, we welcome patients of all ages! Our<br /> team is passionate about building lifetime relationships <br />through positive experiences, featuring:</h6>
+                <h6>Transparent Pricing</h6>
+                <h6>Unparalleled Warranty</h6>
+                <h6>FREE Whitening (for life!)</h6>
+                <br />
+                <br />
+                <h6>“Our word is our worth. We promise to do it right, timely, and for a fair price.”</h6>
+                <br />
+                <br />
+            </div>
+            <div className='col'>
+                <img src={dental} alt="" />
+            </div>
         </div>
-            <div></div>
-        </Container>
 
     );
 };
 
 export default Home;
+
--- src/Components/Services/Servicing.js
+++ src/Components/Services/Servicing.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Servicing.css'
 
 const Servicing = ({ service }) => {
     const { id, name, description, price, img } = service;
     return (
-        <div className='servicing-container pb-3'>
-            <div className='col-md-4 col-xs-6 d-block m-auto'> <img className='img-fluid img-circle' src={img} alt="" /></div>
-            <h4>{name}</h4>
-            <div className="d-block m-auto"><p>{description}</p></div>
-            <h6>{price}</h6>
-            <Link to={`/booking/${id}`}> <button className='btn btn-warning'>Booking Now</button></Link>
+        <div className='servicing-container pb-3 card w-100 m-2 d-flex justify-content-center'>
+            <img className='img-fluid card-img-top' src={img} alt="" />
+            <div className="card-body">
+                <h4 className="card-title">{name}</h4>
+                <p className='card-text'>{description}</p>
+                <h6>{price}</h6>
+                <Link to={`/booking/${id}`}> <button className='btn btn-warning'>Booking Now</button></Link>
+            </div>
+
+
+
         </div>
     );
 };
 
 export default Servicing;
+
+// {/* card */}
+{/* <div class="card" style="width: 18rem;">
+<img src="..." class="card-img-top" alt="...">
+<div class="card-body">
+  <h4 class="card-title">{name}</h4>
+  <p class="card-text">{description}.</p>
+  <a href="{`/booking/${id}`}" class="btn btn-primary">Booking Now</a>
+</div> */}
